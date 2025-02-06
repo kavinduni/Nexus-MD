124cbdb48d9b3a35bbc3626d819a077c4a77ece9
Malvin King <malvink003@gmail.com>
2025-02-06 18:36:41 +0200

    Update handler.js

--- malvin/techlord/handler.js
+++ malvin/techlord/handler.js
@@ -66,7 +66,7 @@ const Handler = async (chatUpdate, sock, logger) => {
         const { isGroup, type, sender, from, body } = m;
         console.log(m);
 
-        const pluginDir = path.join(__dirname, '..', 'Sarkarorder');
+        const pluginDir = path.join(__dirname, '..', 'nexusorder');
         const pluginFiles = await fs.readdir(pluginDir);
 
         for (const file of pluginFiles) {
